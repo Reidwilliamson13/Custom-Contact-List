e3351d11fd511be44d13235bb3cc96ef429fcba9
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-06 15:24:36 -0500

    create useState of contacts

--- custom-contact-list/src/components/App.js
+++ custom-contact-list/src/components/App.js
@@ -1,22 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Reid",
-      email: "reidw19@icloud.com",
-    },
-    {
-      id: "2",
-      name: "Lidia",
-      email: "lidia12@icloud.com",
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
   return (
     <div className="ui container">
       <Header />
