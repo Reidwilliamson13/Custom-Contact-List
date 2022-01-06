be035b121baa17aa15cba2c10e84c4d253fd4e60
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-06 18:11:42 -0500

    add deleteContactHandler

--- custom-contact-list/src/components/ContactList.js
+++ custom-contact-list/src/components/ContactList.js
@@ -4,8 +4,9 @@ import ContactCard from "./ContactCard";
 const ContactList = (props) => {
   console.log(props);
 
+  const deleteContactHandler
   const renderContactList = props.contacts.map((contact) => {
-    return <ContactCard contact={contact}></ContactCard>;
+    return <ContactCard contact={contact} clickHandler={deleteContactHandler}/>;
   });
   return <div className="ui celled list">{renderContactList}</div>;
 };
