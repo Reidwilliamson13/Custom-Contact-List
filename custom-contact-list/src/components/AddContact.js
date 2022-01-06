3e321eb81a59a35db20fa439bb7b703527dcc49c
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-06 11:54:31 -0500

    Add AddContact Component

--- custom-contact-list/src/components/AddContact.js
+++ custom-contact-list/src/components/AddContact.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 const AddContact = () => {
-    return (
-
-    )
-}
-
-
+  return (
+    <div className="ui main">
+      <h2>Add Contact</h2>
+      <form className="ui form">
+        <div className="field">
+          <label>Name</label>
+          <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div className="field">
+          <label>Name</label>
+          <input type="text" name="name" placeholder="Name" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddContact;
