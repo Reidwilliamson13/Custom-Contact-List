155ffa6f428ed653c977d8b3bbcbc574f0cbf7f7
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-07 12:34:44 -0500

    Add Route path

--- custom-contact-list/src/components/App.js
+++ custom-contact-list/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { uuid } from "uuidv4";
 import "./App.css";
 import Header from "./Header";
@@ -34,9 +35,12 @@ function App() {
 
   return (
     <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+      <Router>
+        <Header />
+        <Route path="/add" component={AddContact} />
+        {/* <AddContact addContactHandler={addContactHandler} /> */}
+        {/* <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+      </Router>
     </div>
   );
 }
