07b91ebffebe6f0e977be9927c1bff6bebb9abf0
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-07 14:49:45 -0500

    Add ContactDetail component

---
+++ custom-contact-list/src/components/ContactDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../images/image.svg";
+
+const ContactCard = (props) => {
+  return (
+   
+  );
+};
+
+export default ContactCard;
