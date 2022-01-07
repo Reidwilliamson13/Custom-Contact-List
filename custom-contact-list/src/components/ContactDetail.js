92b0322e9406f6b953801c1b9b8e129ac5c87d05
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-07 15:20:29 -0500

    complete ContactDetail

--- custom-contact-list/src/components/ContactDetail.js
+++ custom-contact-list/src/components/ContactDetail.js
@@ -4,7 +4,17 @@ import user from "../images/image.svg";
 
 const ContactCard = (props) => {
   return (
-   
+    <div className="main">
+      <div className="ui card centered">
+        <div className="image">
+          <img src={user} alt="user" />
+        </div>
+        <div className="content">
+          <div className="header">Reid</div>
+          <div className="description">reidw19@gmail.com</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
